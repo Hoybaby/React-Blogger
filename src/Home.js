0b9bc71b5745a43cf56af73da4f02728c6d01b44
Hoybaby <mbartek436@gmail.com>
2021-01-26 12:38:44 -0500

    getting more practice with useStates

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,8 @@ import React, { useState } from 'react'
 function Home() {
 
     // let name = 'mario';
-    const [name, setName] = useState('mario');
+    const [name, setName] = useState('mario'); //mario is the orinal value at first until changed later by a setName 
+    const [age, setAge] =useState(25);
 
     const handleClick = () => {
         setName('luigi');
@@ -16,7 +17,7 @@ function Home() {
     return (
         <div className="home">
             <h2>Homepage</h2>
-            <p>{name}</p>
+            <p>{name} is {age} old</p>
             <button onClick={handleClick}>Click Me</button>
             {/* <button onClick={(e) => {handleClickAgain('mario', e)}}>Click me again</button> */}
         </div>
