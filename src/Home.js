e7ddb1d045194d655e58651b6c0dea87b7c21ec3
Hoybaby <mbartek436@gmail.com>
2021-01-26 12:49:01 -0500

    removed a ton of code to make it cleaner and to be able to output lists so for later on the blog, it will not have a problem

--- src/Home.js
+++ src/Home.js
@@ -2,25 +2,15 @@ import React, { useState } from 'react'
 
 function Home() {
 
-    // let name = 'mario';
-    const [name, setName] = useState('mario'); //mario is the orinal value at first until changed later by a setName 
-    const [age, setAge] =useState(25);
+    const [blogs, setBlogs] = useState ([
 
-    const handleClick = () => {
-        setName('luigi');
-        setAge('30')
-    }
 
-    // const handleClickAgain = (name, e) => {
-    //     console.log( "hello " + name, e.target);
-    // }
+    ]);
+
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} old</p>
-            <button onClick={handleClick}>Click Me</button>
-            {/* <button onClick={(e) => {handleClickAgain('mario', e)}}>Click me again</button> */}
+            
         </div>
     )
 }
