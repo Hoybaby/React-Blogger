248de46e148ef19adcdc41ceb87a5c58f041e3a2
Hoybaby <mbartek436@gmail.com>
2021-01-26 11:37:13 -0500

    trying to see if i can reveal a message in the console with a button

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 function Home() {
+
+    const handleClick = () => {
+        console.log("test works");
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button>Click Me</button>
             
         </div>
     )
