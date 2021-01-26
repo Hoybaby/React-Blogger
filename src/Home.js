8e54775bff497063ed2762433d79a0b159d46687
Hoybaby <mbartek436@gmail.com>
2021-01-26 11:57:19 -0500

    trying to change the name of mario to luigi

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,22 @@ import React from 'react'
 
 function Home() {
 
+    let name = 'mario';
+
     const handleClick = (e) => {
-        console.log("test works", e);
+        name = 'luigi';
     }
 
-    const handleClickAgain = (name, e) => {
-        console.log( "hello " + name, e.target);
-    }
+    // const handleClickAgain = (name, e) => {
+    //     console.log( "hello " + name, e.target);
+    // }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name}</p>
             <button onClick={handleClick}>Click Me</button>
-            <button onClick={(e) => {handleClickAgain('mario', e)}}>Click me again</button>
+            {/* <button onClick={(e) => {handleClickAgain('mario', e)}}>Click me again</button> */}
         </div>
     )
 }
