a6a4bdab3960d94cf4ade804c1e7db4e4a436896
Hoybaby <mbartek436@gmail.com>
2021-01-28 13:22:19 -0500

    use the useEffect to fetch the data i made to reveal it. Also i need to make more notes in my code for others to follow along and myself at a later point

--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,7 @@ import BlogList from './BlogList.jsx';
 
 function Home() {
 
-    const [blogs, setBlogs] = useState ([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState (null);
 
 
     const [name, setName] = useState('mario');
@@ -23,7 +19,7 @@ function Home() {
     useEffect(() => {
         console.log('use effect ran');
         console.log(name);
-    }, [name])
+    }, [])
 
     return (
         <div className="home">
