d17ab6ac1633e544a3bb79e0bbbd912f68a22d1f
Hoybaby <mbartek436@gmail.com>
2021-02-25 23:36:12 -0500

    trying to add more

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -5,12 +5,22 @@ const BlogDetails =() => {
 
     // we name it ID because we nammed it that in app.js
     const{id}= useParams();
-    const {data, error, isLoading} = useFetch('http://localhost:8000/blogs/' +id);
+    const {data:blog, error, isLoading} = useFetch('http://localhost:8000/blogs/' +id);
 
     return (
         
         <div className="blog-details">
             <h2>Blog Details - {id} </h2>
+            {isLoading && <div>Loading...</div>}
+            {error && <div> {error} </div>}
+            {blog && (
+                <article>
+                    {/* this data is from our data segment in db.json */}
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
     );
 }
