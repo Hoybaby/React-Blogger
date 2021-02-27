4628517cf1c0a29273998ae9835ab157f4d905c5
Hoybaby <mbartek436@gmail.com>
2021-02-27 18:03:59 -0500

    making some forms for this blog website with react

--- src/Create.jsx
+++ src/Create.jsx
@@ -4,6 +4,22 @@ const Create = () => {
     return (
         <div className="create">
             <h2>Add a New blog!</h2>
+            <form>
+                <label>Blog Title: </label>
+                <input 
+                type="text"
+                required
+                />
+                <label>Blog Body: </label>
+                <textarea 
+                required
+                ></textarea>
+                <label>Blog Aurthor: </label>
+                    <select>
+                        <option value="mario">mario</option>
+                        <option value="yoshi">yoshi</option>
+                    </select>
+            </form>
         </div>
         );
 }
